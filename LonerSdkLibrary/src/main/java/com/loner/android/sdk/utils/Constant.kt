95ead9df55082a9d2baded5a9038860a7e8f4a96
anandl-cuelogic <anand.lilhare@cuelogic.com>
2019-07-22 13:02:42 +0530

    add new access token and device id

--- LonerSdkLibrary/src/main/java/com/loner/android/sdk/utils/Constant.kt
+++ LonerSdkLibrary/src/main/java/com/loner/android/sdk/utils/Constant.kt
@@ -7,8 +7,10 @@ object Constant {
     const val STASK_MESSAGE = 3
     const val STASK_NOTIFICATION = 4
 
-    const val accessToken ="813af900bd61a5d2d0cf"
-    const val deviceId:String ="800006506"
+    const val accessToken ="ce0455f92dbcd62b0369"
+    const val deviceId:String ="800006503"
 
     const val DIM_VALUE_FOR_DIALOG = 0.3f
+
+
 }
