a49497c6f3f999b1e17c74d890c49edd4cfcbe90
anandl-cuelogic <anand.lilhare@cuelogic.com>
2019-09-13 15:24:19 +0530

    working on mvvm model in sdk

---
+++ LonerSdkLibrary/src/main/java/com/loner/android/sdk/viewModel/AlertActivityViewModel.kt
@@ -0,0 +1,12 @@
+package com.loner.android.sdk.viewModel
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+
+
+class AlertActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+
+
+    
+}
